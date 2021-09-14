5cf7e70605bd27e60b7143b8201a691f3273afcb
Bondok6 <kirolousbondok6@gmail.com>
2021-09-14 15:30:00 +0200

    Geolocation Api

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// Geolocation Api -> is a browser api
+navigator?.geolocation.getCurrentPosition(
+  function (pos) {
+    // console.log(pos);
+    const { latitude } = pos.coords;
+    const { longitude } = pos.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  },
+  function () {
+    alert("Could not acess you position");
+  }
+);
